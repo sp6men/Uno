236fcfad6079fe9caa332bf292324e2e9ecd574e
sp6men <37633117+sp6men@users.noreply.github.com>
2018-11-26 07:09:16 +0100

    Add files via upload

---
+++ uno/uno_final/nouvellelogique.js
@@ -0,0 +1,7 @@
+/*Pour une carte du joueur 1*/
+
+LEBOUTON.onclick = function nouvellelogique(){
+        let a = CARTE1J1.splice(0,1)
+    if (testTABLEAU.length == 0 && testTABLEAU3.length == 0)
+         
+}
